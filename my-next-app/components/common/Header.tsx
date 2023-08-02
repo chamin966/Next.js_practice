0896c0fc596c71e9ba24cbf89002a6d380eee8f4
Minchan <98478661+chamin966@users.noreply.github.com>
2023-08-02 12:51:45 +0900

    section3-2

--- my-next-app/components/common/Header.tsx
+++ my-next-app/components/common/Header.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '../../styles/header.module.scss';
 import Image from 'next/image';
+import styles from '../../styles/header.module.scss';
 
 interface Props {
+  onClickLogo?: () => void;
   rightElements?: React.ReactElement[];
 }
 
-const Header = ({ rightElements }: Props) => {
+const HeaderComponent = ({ onClickLogo, rightElements }: Props) => {
   return (
     <header className={styles.header}>
       <div className={styles.flexItem}>
-        <Link href={'/'} className={styles.box}>
+        <Link href="/" onClick={onClickLogo} className={styles.box}>
           <Image
             src="/inflearn.png"
-            alt={'인프런 로고'}
             width={110}
             height={20}
+            alt="인프런 로고"
           />
         </Link>
       </div>
@@ -25,4 +26,4 @@ const Header = ({ rightElements }: Props) => {
   );
 };
 
-export default Header;
+export default HeaderComponent;
