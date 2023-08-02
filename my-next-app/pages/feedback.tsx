3141d67b1f714d0d68dadc5f7897fb6683d0c419
Minchan <98478661+chamin966@users.noreply.github.com>
2023-08-02 15:40:48 +0900

    section5

--- my-next-app/pages/feedback.tsx
+++ my-next-app/pages/feedback.tsx
@@ -1,33 +1,16 @@
 import { Fragment } from 'react';
-import styles from '../styles/header.module.scss';
 import Header from '../components/common/Header';
-import Link from 'next/link';
-import { AiOutlineShareAlt } from 'react-icons/ai';
-import { VscFeedback } from 'react-icons/vsc';
+import { NextSeo } from 'next-seo';
 
-function Feedback() {
+export default function Feedback() {
   return (
     <Fragment>
-      <Header
-        rightElements={[
-          <button
-            className={styles.box}
-            key="btn" // jsx 배열에는 요소마다 유일한 키값이 필요함
-            style={{ marginRight: 8 }}
-            onClick={() => {
-              alert('복사');
-            }}
-          >
-            <AiOutlineShareAlt size={20} />
-          </button>,
-          <Link key="link" className={styles.box} href="/">
-            <VscFeedback size={20} />
-          </Link>,
-        ]}
+      <NextSeo
+        title="피드백"
+        description="매장 지도 서비스에 대한 피드백을 받습니다."
       />
-      <main></main>
+      <Header />
+      <main>피드백 페이지입니다.</main>
     </Fragment>
   );
 }
-
-export default Feedback;
